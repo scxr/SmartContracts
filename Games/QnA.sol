937fcbf4fb9da349e8b77a3c21ff3c84095f0a48
n6j <cswilson@zoho.com>
2021-09-29 00:13:57 +0100

    added qna

---
+++ Games/QnA.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// @Author: nemo
+
+pragma solidity^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract QnA is Ownable {
+    mapping(uint256 => bytes32) questionsAns;
+    
+    function addQuestion(uint256 qNumber, bytes32 answer) public onlyOwner{
+        questionsAns[qNumber] = answer;
+    }
+    
+    function ansQuestionString(string memory answer, uint256 qNumber) public view returns(bool) {
+        bytes32 answerBytes = keccak256(abi.encodePacked(answer));
+        return questionsAns[qNumber] == answerBytes;
+    }
+    
+    function ansQuestionBytes(bytes32 answer, uint256 qNumber) public view returns(bool){
+        return questionsAns[qNumber] == answer;
+    }
+    
+    function getBytes(string memory toHash) public pure returns(bytes32) {
+        return keccak256(abi.encodePacked(toHash));
+    }
+      
+}
